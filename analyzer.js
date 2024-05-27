124056151c507568be807b651e6dfb7e4cd7a350
Aben <126703861+abengl@users.noreply.github.com>
2024-05-27 16:33:33 -0500

    Updates

---
+++ analyzer.js
@@ -0,0 +1,76 @@
+const analyzer = {  
+  getWordCount: (text) => {
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordsArray = text.split(" ");
+
+    return wordsArray.length;
+  },
+  getCharacterCount: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const charArray = text.split("");
+
+    return charArray.length;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+     
+    const pattern = /[\p{P}|\s]/gu; // Este patrón identifica todo carácter de puntuación (patrón unicode) o espacio en blanco
+
+    const newText = text.replace(pattern, "");
+
+    return newText.length;
+  },
+  getAverageWordLength: (text) => {    
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordsArray = text.split(" "); 
+    let sum = 0;
+    
+    // Uso un bucle para obtener la longitud de cada palabra y sumarla
+    for (let i = 0; i < wordsArray.length; i++) {
+      sum += wordsArray[i].length;
+    }
+    // Calculo el promedio de la longitud de las palabras
+    const average = sum/wordsArray.length;
+
+    // Si el promedio no es un número entero, lo redondeo a dos decimales y lo vuelvo a convertir a número, de caso contrario, lo retorno tal cual
+    if(!Number.isInteger(average)){
+      return Number(average.toFixed(2));
+    }
+    else{
+      return average;
+    }
+  },
+  getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const textArray = text.split(" ");
+    const pattern = /^\d+(\.\d+\.?)?$/; // Este patrón identifica todo número entero o decimal que termine con un punto o no.
+    let counter = 0;
+
+    // Uso un bucle para recorrer cada palabra del array y verificar si contiene el patrón, de ser así incremento el contador
+    for (let i = 0; i < textArray.length; i++) {
+      const word = textArray[i];
+      if (pattern.test(word)) {
+        counter++;
+      }
+    }
+    return counter;
+  },
+  getNumberSum: (text) => {
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
+    const textArray = text.split(" ");
+    const pattern = /^\d+(\.\d+.?)?$/; // Cualquier número entero (12) o decimal (1.2) que termine con un punto o no.
+    let numTotal = 0;
+    
+    for (let i = 0; i < textArray.length; i++) {
+      const word = textArray[i];
+      if (pattern.test(word)){
+        numTotal += parseFloat(word);
+      }
+    }
+    return numTotal;
+    
+  },
+};
+
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,53 @@
+import analyzer from './analyzer.js';
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// Accedo al elemento <ul> que contiene los <li> en el DOM
+const ulElement = document.querySelector("ul");
+
+// Accedo a los elementos <li> de forma global
+const liElements = ulElement.querySelectorAll("li")
+
+// Asigno una variable específica a cada <li> para poder modificar su contenido
+const wordCounterLi = liElements[0];
+const charCounterLi = liElements[1];
+const numCounterLi = liElements[2];
+const charWithoutSpacesCounterLi = liElements[3];
+const averageLengthCounterLi = liElements[4];
+const sumCounterLi = liElements[5];
+
+// Accedo al <textarea> que contiene el texto ingresado por la usuaria
+const textArea =document.querySelector("[name='user-input']");
+
+// Asigno un Event Listener para escuchar el evento cuando se genera un "input" en el <textarea> para ejecutar la función updateMetricas
+textArea.addEventListener("input", updateMetricas);
+
+// La aplicación actualiza el atributo textContent o innerHTML de los <li> que mostrar las métricas del texto.
+function updateMetricas(){
+
+  // Obtengo el valor del texto ingresado en el textArea por la usuaria
+  const textInput = textArea.value;
+
+  // Defino variables que contienen el resultado de cada método del objeto analyzer
+  const wordCounter = analyzer.getWordCount(textInput);
+  const charCounter = analyzer.getCharacterCount(textInput);
+  const charCounterWithoutSpaces = analyzer.getCharacterCountExcludingSpaces(textInput);
+  const numberCounter = analyzer.getNumberCount(textInput);
+  const averageWordLength = analyzer.getAverageWordLength(textInput);
+  const sumCounter = analyzer.getNumberSum(textInput);
+
+  // Modifico el contenido de cada <li> con el resultado de las métricas almacenados en las variables previas
+  wordCounterLi.textContent = "Palabras: " + wordCounter;
+  charCounterLi.textContent = "Caracteres: " +charCounter;
+  numCounterLi.textContent = "Números: " + numberCounter;
+  charWithoutSpacesCounterLi.textContent = "Caracteres sin Espacios: " + charCounterWithoutSpaces;
+  averageLengthCounterLi.textContent = "Promedio de Longitud: " + averageWordLength;
+  sumCounterLi.textContent = "Suma de Números: " + sumCounter;
+}
+
+
+// Accedo al elemento <button> en el DOM
+const button = document.getElementById("reset-button");
+
+// Asigno un Event Listener para escuchar el evento "click" en el <button> y resetear el valor del TextArea
+button.addEventListener("click", () => { textArea.value = "" });
